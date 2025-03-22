41c2eabdf5eb0b79654dcd214b8e954bb2802a06
Kabir Bose <kabirbose04@gmail.com>
2025-03-22 18:00:34 -0400

    created basic langchain workflow

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 "use strict";
 
 import "dotenv/config";
+import { ChatGroq } from "@langchain/groq";
+
+const llm = new ChatGroq({
+  model: "llama-3.3-70b-versatile",
+  temperature: 0,
+});
+
+const input = `Translate "I love programming" into French.`;
+
+const invokeModel = async (input: string) => {
+  try {
+    return await llm.invoke(input);
+  } catch (error) {
+    console.error("Error invoking model:", error);
+    return "An error occurred while processing the request.";
+  }
+};
+
+(async () => {
+  const response = await invokeModel(input);
+  console.log(response);
+})();
