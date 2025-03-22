b69aa446fb598ea9932530c51bfb0f27fc28f227
Kabir Bose <kabirbose04@gmail.com>
2025-03-21 22:08:29 -0400

    implemented firecrawl

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@
 
 import { ChatGroq } from "@langchain/groq";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import FirecrawlApp, {
+  CrawlParams,
+  CrawlStatusResponse,
+} from "@mendable/firecrawl-js";
+
 import "dotenv/config";
 
 const model = new ChatGroq({
@@ -9,13 +14,30 @@ const model = new ChatGroq({
   temperature: 0,
 });
 
-async function query() {
+const fc = new FirecrawlApp({ apiKey: process.env.FC_API_KEY });
+
+async function qroqQuery() {
   const messages = [
     new SystemMessage("Translate the following from English into Italian"),
     new HumanMessage("hi!"),
   ];
 
   const message = await model.invoke(messages);
-  console.log(message.content);
+  return message;
 }
-// query();
+
+async function scrapeSite(url: string) {
+  const scrapeResponse = await fc.scrapeUrl(url, {
+    formats: ["markdown", "html"],
+  });
+
+  if (scrapeResponse) {
+    console.log(scrapeResponse);
+  }
+}
+
+// groqQuery();
+
+scrapeSite(
+  "https://jobs.ashbyhq.com/snowflake/152b293d-ec65-4862-8957-7f50089728ac/application?gh_src=ed5543a62&utm_source=Q2P9NP2NNP&utm_medium=phenom-feeds"
+);
